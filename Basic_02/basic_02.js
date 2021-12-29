753fb48b3eedeccd89c28ca468f006f798fc900e
Alimul-Mahfuz <alimulmahfuztushar@gmail.com>
2021-12-29 19:57:01 +0600

    second commit

---
+++ Basic_02/basic_02.js
@@ -0,0 +1,40 @@
+//Creating an Array
+
+var myArray = [
+    'Tushar', 'nafisa','hossain','shobuz'
+]
+console.log(myArray[1]);
+
+//Objects in an array
+var objArray = {
+    'name': ['tushar','nafisa'],
+    'age' : [33,34],
+    'text': 'Integer'
+};
+
+console.log(objArray['age'][1]);
+
+var objArrayTwo =[
+    {
+        'name':"QuancyViolet",
+        'room':34,
+        'location':'Dhaka',
+        'booked':28
+    },
+    {
+        'name':'Volkwagon',
+        'seats':4,
+        'color':'black',
+        'engine': '1200HP',
+        'wheels':4,
+    },
+    
+];
+//In JavaScript, arrays use numbered indexes.  
+
+// In JavaScript, objects use named indexes.
+console.log(typeof(objArrayTwo[0].name));
+
+var resulation = window.screen.width;
+console.log(resulation);
+alert(document.lastModified);
